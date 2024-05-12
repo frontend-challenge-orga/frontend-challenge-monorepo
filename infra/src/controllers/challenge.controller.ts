1d5afb5980017645d3d529c6ad335a786de5bd85
SwiichyCode <adlpromail@gmail.com>
2024-05-12 09:08:01 +0200

    config: setup nodemon with typescript

--- infra/src/controllers/challenge.controller.ts
+++ infra/src/controllers/challenge.controller.ts
@@ -7,7 +7,7 @@ export const ChallengeController = (challengeApi: ChallengeApi) => {
   app.get(CHALLENGE_ENDPOINTS.CHALLENGES, async (_, res: Response) => {
     try {
       const challenges = await challengeApi.getChallenges();
-      res.json(challenges);
+      res.status(200).json(challenges);
     } catch (error) {
       console.error(error);
     }
